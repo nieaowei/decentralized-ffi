891b6cd2175b0e4aff5c67588d73b7d95bfcc3ad
Nekilc <nieaowei@gmail.com>
2025-06-23 00:48:02 +0800

    Added extract_contact_from_csv_data

--- decentralized-ffi/src/utils.rs
+++ decentralized-ffi/src/utils.rs
@@ -1,26 +1,28 @@
+use crate::bitcoin::{Address, Script, TxIn, TxOut};
+use base64::prelude::BASE64_STANDARD;
+use base64::Engine;
+use bdk_wallet::bitcoin::consensus::encode::deserialize_hex;
 use std::collections::HashMap;
 use std::str::FromStr;
 use std::sync::Arc;
-use base64::Engine;
-use base64::prelude::BASE64_STANDARD;
-use bdk_wallet::bitcoin::consensus::encode::deserialize_hex;
-use crate::bitcoin::{Script, TxIn, TxOut};
 
-use bdk_wallet::bitcoin::{TxIn as BDKTxIn, Witness};
+use crate::testnet4::Network;
 use bdk_wallet::bitcoin::TxOut as BDKTxOut;
+use bdk_wallet::bitcoin::{TxIn as BDKTxIn, Witness};
 use bdk_wallet::serde_json;
 use chrono::SecondsFormat;
 use hmac::{Hmac, KeyInit, Mac};
 use jsonpath_rust::JsonPath;
 use regex::Regex;
+use serde::Deserialize;
 use sha2::Sha256;
+use uniffi::Record;
 use url::Url;
 
 #[uniffi::export]
 pub fn script_to_asm_string(script: Arc<Script>) -> String {
     script.0.to_asm_string()
 }
-
 
 #[uniffi::export]
 pub fn new_txin_from_hex(hex: String, witness_hex: String) -> Option<TxIn> {
@@ -34,9 +36,10 @@ pub fn new_txin_from_hex(hex: String, witness_hex: String) -> Option<TxIn> {
 
 #[uniffi::export]
 pub fn new_txout_from_hex(hex: String) -> Option<TxOut> {
-    deserialize_hex::<BDKTxOut>(hex.as_str()).ok().map(|e| (&e).into())
+    deserialize_hex::<BDKTxOut>(hex.as_str())
+        .ok()
+        .map(|e| (&e).into())
 }
-
 
 #[derive(Debug, thiserror::Error, uniffi::Error)]
 pub enum UtilsError {
@@ -47,9 +50,13 @@ pub enum UtilsError {
     JsonPath { error_message: String },
 }
 
-
 #[uniffi::export]
-pub fn get_json_info_from_url(url: String, auth: String, params: Vec<String>, paths: Vec<String>) -> Result<Vec<Option<String>>, UtilsError> {
+pub fn get_json_info_from_url(
+    url: String,
+    auth: String,
+    params: Vec<String>,
+    paths: Vec<String>,
+) -> Result<Vec<Option<String>>, UtilsError> {
     let mut url = url;
     for param in params.iter().enumerate() {
         url = url.replace(&format!(r#"{{{}}}"#, param.0), param.1);
@@ -59,7 +66,10 @@ pub fn get_json_info_from_url(url: String, auth: String, params: Vec<String>, pa
     let resp = minreq::get(url.as_str());
 
     let now = chrono::Utc::now().to_rfc3339_opts(SecondsFormat::Millis, true);
-    let auths: HashMap<String, String> = serde_json::from_str(&auth).map_err(|err| UtilsError::JsonPath { error_message: err.to_string() })?;
+    let auths: HashMap<String, String> =
+        serde_json::from_str(&auth).map_err(|err| UtilsError::JsonPath {
+            error_message: err.to_string(),
+        })?;
     let mut headers = HashMap::new();
     for (key, mut value) in auths {
         if key.to_lowercase() == "AUTHORIZATION".to_lowercase() {
@@ -67,7 +77,10 @@ pub fn get_json_info_from_url(url: String, auth: String, params: Vec<String>, pa
         } else {
             value = value.replace("{timestamp}", now.as_str());
             value = value.replace("{request_method}", "GET");
-            value = value.replace("{request_path}", &format!("{}?{}", url.path(), url.query().unwrap_or_default()));
+            value = value.replace(
+                "{request_path}",
+                &format!("{}?{}", url.path(), url.query().unwrap_or_default()),
+            );
 
             let reg = Regex::new(r"hmac\((.*)\)").unwrap();
             let data = reg.captures(&value);
@@ -91,16 +104,32 @@ pub fn get_json_info_from_url(url: String, auth: String, params: Vec<String>, pa
             headers.insert(key, value);
         }
     }
-    let json = resp.with_headers(headers).send().map_err(|e| UtilsError::Minreq { error_message: e.to_string() })?;
+    let json = resp
+        .with_headers(headers)
+        .send()
+        .map_err(|e| UtilsError::Minreq {
+            error_message: e.to_string(),
+        })?;
     if json.status_code != 200 {
-        return Err(UtilsError::Minreq { error_message: json.as_str().unwrap_or_default().to_string() });
+        return Err(UtilsError::Minreq {
+            error_message: json.as_str().unwrap_or_default().to_string(),
+        });
     }
-    let json = json.as_str().map_err(|e| UtilsError::JsonPath { error_message: e.to_string() })?;
+    let json = json.as_str().map_err(|e| UtilsError::JsonPath {
+        error_message: e.to_string(),
+    })?;
 
     let mut datas = Vec::new();
     for path in paths {
-        let path = JsonPath::from_str(path.as_str()).map_err(|e| UtilsError::JsonPath { error_message: e.to_string() })?;
-        let data = path.find(&serde_json::Value::from_str(&json).map_err(|e| UtilsError::JsonPath { error_message: e.to_string() })?);
+        let path = JsonPath::from_str(path.as_str()).map_err(|e| UtilsError::JsonPath {
+            error_message: e.to_string(),
+        })?;
+        let data =
+            path.find(
+                &serde_json::Value::from_str(&json).map_err(|e| UtilsError::JsonPath {
+                    error_message: e.to_string(),
+                })?,
+            );
         if !data.is_null() {
             if let Some(data) = data.as_array() {
                 if let Some(data) = data.first() {
@@ -120,7 +149,33 @@ pub fn get_json_info_from_url(url: String, auth: String, params: Vec<String>, pa
     Ok(datas)
 }
 
+#[derive(Deserialize, Record)]
+pub struct Contact {
+    #[serde(alias = "Label")]
+    pub label: String,
+    #[serde(alias = "Address")]
+    pub address: String,
+}
 
+#[uniffi::export]
+pub fn extract_contact_from_csv_data(
+    csv_data: Vec<u8>,
+    network: Network,
+) -> Result<Vec<Contact>, UtilsError> {
+    let mut csv = csv::Reader::from_reader(csv_data.as_slice());
+    let mut contacts = vec![];
+    for record in csv.deserialize::<Contact>() {
+        match record {
+            Ok(contact) => {
+                if let Ok(addr) = Address::new(contact.address.clone(), network) {
+                    contacts.push(contact);
+                }
+            }
+            Err(err) => {}
+        }
+    }
+    Ok(contacts)
+}
 #[cfg(test)]
 mod tests {
     fn get_esplora_client() -> crate::esplora::EsploraClient {
